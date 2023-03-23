0b66b302b36756483ddfe2ace560e7685d0fb590
techysiddhant <sidjain8000@gmail.com>
2023-03-23 13:32:06 +0530

    commit

---
+++ react_jsx_and_props/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./header.css";
+const Header = () => {
+  return (
+    <nav>Header</nav>
+  )
+}
+
+export default Header
---
+++ react_jsx_and_props/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React,{useState,useEffect} from 'react'
+
+const Home = () => {
+    // use state hook
+    // const [inputValue,setInputValue] = useState(0);
+    // useEffect(() => {
+
+    // }, )
+    
+  return (
+    <div>
+        <input type="text" placeholder='Enter Some Thing' />
+
+    </div>
+  )
+}
+
+export default Home
